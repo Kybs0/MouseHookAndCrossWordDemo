dc813da41be1f9ff71f2e50a796148eba7a5e7fa
kybs0 <1016724958@qq.com>
2020-07-19 21:57:54 +0800

    添加项目文件。

---
+++ MouseHookDemo/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Threading;
+using Gma.System.MouseKeyHook;
+
+namespace MouseHookDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IntPtr _hWndNextViewer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var mouseHookManager = new MouseHookManager();
+            mouseHookManager.Init();
+            Initialize();
+        }
+        public void Initialize()
+        {
+            var wih = new WindowInteropHelper(this);
+            var hWndSource = HwndSource.FromHwnd(wih.Handle);
+            if (hWndSource != null)
+            {
+                hWndSource.AddHook(this.WinProc); // start processing window messages
+                _hWndNextViewer = Win32.SetClipboardViewer(hWndSource.Handle); // set this window as a viewer
+            }
+        }
+
+        private IntPtr WinProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case Win32.WmChangecbchain:
+                    //Debug.WriteLine("WinProc,Win32.WmChangecbchain");
+                    if (wParam == _hWndNextViewer)
+                        this._hWndNextViewer = lParam; //clipboard viewer chain changed, need to fix it.
+                    else if (_hWndNextViewer != IntPtr.Zero)
+                        Win32.SendMessage(_hWndNextViewer, msg, wParam, lParam); //pass the message to the next viewer.
+                    break;
+                case Win32.WmDrawclipboard:
+                    Win32.SendMessage(_hWndNextViewer, msg, wParam, lParam); //pass the message to the next viewer //clipboard content changed
+                    if (Clipboard.ContainsText() && !string.IsNullOrEmpty(Clipboard.GetText().Trim()))
+                    {
+                        Application.Current.Dispatcher.Invoke(
+                            DispatcherPriority.Background,
+                            (Action)
+                                delegate
+                                {
+                                    var currentText = Clipboard.GetText().Trim();
+                                    if (!string.IsNullOrEmpty(currentText))
+                                    {
+                                        MyTestTextBlock.Text = currentText;
+                                    }
+                                });
+                    }
+                    break;
+            }
+
+            return IntPtr.Zero;
+        }
+    }
+}
---
+++ MouseHookDemo/MouseHookManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+
+namespace MouseHookDemo
+{
+    public class MouseHookManager
+    {
+        private IKeyboardMouseEvents globalMouseHook;
+        private bool isMouseDown;
+        private Point mouseSecondPoint;
+        private Point mouseFirstPoint;
+
+        public void Init()
+        {
+            globalMouseHook = Hook.GlobalEvents();
+            globalMouseHook.MouseDoubleClick += async (o, args) => await this.MouseDoubleClicked(o, args);
+            globalMouseHook.MouseDown += async (o, args) => await this.MouseDown(o, args);
+            globalMouseHook.MouseUp += async (o, args) => await this.MouseUp(o, args);
+        }
+
+        private async Task MouseUp(object sender, MouseEventArgs e)
+        {
+            //Debug.WriteLine($"MouseUp:{e.Location}");
+            this.mouseSecondPoint = e.Location;
+
+            if (this.isMouseDown && !this.mouseSecondPoint.Equals(this.mouseFirstPoint))
+            {
+                await Task.Run(() =>
+                {
+                    SendKeys.SendWait("^c");
+                    //Debug.WriteLine($"MouseUp:SendKeys.SendC;");
+                });
+                this.isMouseDown = false;
+            }
+            this.isMouseDown = false;
+        }
+
+        private async Task MouseDown(object sender, MouseEventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                this.mouseFirstPoint = e.Location;
+                this.isMouseDown = true;
+                //Debug.WriteLine($"MouseDown:{e.Location}");
+            });
+        }
+
+        private async Task MouseDoubleClicked(object sender, MouseEventArgs e)
+        {
+            //Debug.WriteLine($"MouseDoubleClicked");
+            this.isMouseDown = false;
+            await Task.Run(() =>
+            {
+                SendKeys.SendWait("^c");
+                //Debug.WriteLine($"MouseDoubleClicked:SendKeys.SendC");
+            });
+        }
+    }
+}
